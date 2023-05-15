5c840752e9f04f54302541fdf797af5f89f21fea
Nicolas <nicolas.went@epita.fr>
2023-05-15 16:38:09 +0200

    [Conf] Added config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ author = 'Nicolas Went'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -27,5 +27,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Google analytics can be added to the theme:
+# https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
